94ec8617d8f0deea0d05f38a733bf18118a90614
Rich Jones <rich@anomos.info>
2014-04-14 01:18:46 -0700

    relic start

--- relic.py
+++ relic.py
@@ -1,7 +1,27 @@
-import re
+from jinja2 import Environment, FileSystemLoader
+from clint import arguments
 import os
-import jinja2
-import clint
+import json
+
+# Get demands
+args = arguments.Args()
+demands_filename = args.get(0)
+demands_file = open(demands_filename)
+demands_lines = demands_file.readlines()
+try:
+    demands = json.loads(demands_lines)
+except Exception, e:
+    print e
+    print demands_lines
+    print "Could not parse demands."
 
 
+# Set up law
+env = Environment(loader=FileSystemLoader(''))
+template = env.get_template('default_resolution.html')
+output_from_parsed_template = template.render(demands)
+print output_from_parsed_template
 
+# Write the law
+with open(demands_filename.replace('.html', '.txt'), "wb") as fh:
+        fh.write(output_from_parsed_template)
