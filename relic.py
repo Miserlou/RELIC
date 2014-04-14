d47500378956be3dc75dfed1dbccb084a08d02b6
Rich Jones <rich@anomos.info>
2014-04-14 10:14:07 -0700

    cont

--- relic.py
+++ relic.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python
+
 from jinja2 import Environment, FileSystemLoader
 from clint import arguments
 import os
@@ -6,10 +8,12 @@ import json
 # Get demands
 args = arguments.Args()
 demands_filename = args.get(0)
-demands_file = open(demands_filename)
-demands_lines = demands_file.readlines()
+with open(demands_filename, "r") as demands_file:
+    demands_lines=demands_file.read().replace('\n', '')
+
 try:
     demands = json.loads(demands_lines)
+    print json.dumps(demands, indent=4)
 except Exception, e:
     print e
     print demands_lines
@@ -18,10 +22,10 @@ except Exception, e:
 
 # Set up law
 env = Environment(loader=FileSystemLoader(''))
-template = env.get_template('default_resolution.html')
+template = env.get_template('default_resolution.law')
 output_from_parsed_template = template.render(demands)
 print output_from_parsed_template
 
 # Write the law
-with open(demands_filename.replace('.html', '.txt'), "wb") as fh:
+with open(demands_filename.replace('.demands', '.txt'), "wb") as fh:
         fh.write(output_from_parsed_template)
